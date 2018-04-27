702c3d4948d95ecf15d43debca4ed9d651abf95b
romcheck <romcheck@gmail.com>
2018-04-27 13:01:55 +0200

    upd writeback index

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cat <<EOF > config.yaml
-
 es_host: ${ES_HOST:-elasticsearch}
 rules_folder: rules
 es_port: ${ES_PORT:-9200}
@@ -9,7 +8,7 @@ run_every:
   seconds: ${RUN_EVERY_SECONDS:-5}
 buffer_time:
   minutes: ${BUFFER_TIME_MINUTES:-1}
-writeback_index: elastalert_status
+writeback_index: elastalert
 EOF
 
 exec "$@"
